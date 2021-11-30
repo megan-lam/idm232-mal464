700b5571babfb253ed271de7656bdc7233df23f8
megan-lam <72055777+megan-lam@users.noreply.github.com>
2021-11-30 13:28:55 -0500

    yay

--- admin/recipe/create.php
+++ admin/recipe/create.php
@@ -6,13 +6,15 @@ include_once '../../global/admin_header_black.php'; ?>
 if (isset($_POST['submit'])) {
     //  Parse Data
     $recipe_title = mysqli_real_escape_string($db_connection, $_POST['recipe_title']);
+    $category = mysqli_real_escape_string($db_connection, $_POST['category']);
     $ingredients = mysqli_real_escape_string($db_connection, $_POST['ingredients']);
     $steps = mysqli_real_escape_string($db_connection, $_POST['steps']);
+    $current_date = getFormattedDateTime();
     
 
     // Build Query
-    $sql = "INSERT INTO `Recipes` (`recipe_title`, `ingredients`, `steps`)
-    VALUES ('$recipe_title', '$ingredients', '$steps');";
+    $sql = "INSERT INTO `Recipes` (`recipe_title`, `category`, `ingredients`, `steps`, `date_posted`, `date_updated`)
+    VALUES ('$recipe_title', '$category','$ingredients', '$steps', '{$current_date}', '{$current_date}');";
 
     // Execute Query
     $db_results = mysqli_query($db_connection, $sql);
@@ -36,9 +38,12 @@ if (isset($_POST['submit'])) {
         <input class="add_pic btn" type="file" id="myFile" name="filename">
      
         <div class="mobile_center">
-            <div class="center_recipe">
-                <div class="column left">
-
+            <div class="cat_layout">
+            <input class="info" id="category" type="text" name="category" value="" placeholder="+ Category" required>
+            </div>
+        <div class="center_recipe">
+            
+        <div class="column left">
         <h2 class="ingredients">INGREDIENTS</h2>
             <input class="info" id="ingredient" type="text" name="ingredients" value="" placeholder="+ Add Ingredient" required>
         </div>
